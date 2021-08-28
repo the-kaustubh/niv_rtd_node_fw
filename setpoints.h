fc72c896308935d1f80c943e21e3d1f4f3bc50e9
Kaustubh Murumkar <kaustubh.murumkar@gmail.com>
2021-08-28 22:00:43 +0530

    format json debug prints

--- setpoints.h
+++ setpoints.h
@@ -17,10 +17,22 @@ void fetchSetpoint() {
 
   DynamicJsonDocument json(1024);
   deserializeJson(json, response);
+  String ddata;
+  ddata = json['temperaturemin'].as<const char*>();
+  ddata += ", ";
+  ddata += json['temperaturemax'].as<const char*>();
+  ddata += "\n";
+  ddata += json['humiditymin'].as<const char*>();
+  ddata += ", ";
+  ddata += json['humiditymax'].as<const char*>();
+  ddata += "\n";
+  ddata += json['co2min'].as<const char*>();
+  ddata += ", ";
+  ddata += json['co2max'].as<const char*>();
+  ddata += "\n";
+
   Serial.println("+++++++++JSON DATA++++++++++");
-  Serial.println(json['temperaturemin'] + "," + json['temperaturemax']);
-  Serial.println(json['humiditymin'] + "," + json['humiditymax']);
-  Serial.println(json['co2min'] + "," + json['co2max']);
+  Serial.println(ddata);
   Serial.println("++++++++++++++++++++++++++++");
 
   updateSetPoint(TEMP_MIN_ADDR, json['temperaturemin'], json['temperaturemax']);
